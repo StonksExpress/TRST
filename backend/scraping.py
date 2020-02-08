08246e9f225aa199135bc8d87f6d6a27127be0a2
Fiwo735 <fiwo735@gmail.com>
2020-02-08 23:34:21 +0000

    more websites available for scraping

--- backend/scraping.py
+++ backend/scraping.py
@@ -1,27 +1,66 @@
 from bs4 import BeautifulSoup
 import sys
 import requests
+import re
+
+
+
 
 def scraping(websiteName):
     response = requests.get(websiteName)
     soup = BeautifulSoup(response.content, "html.parser")
     elements=[]
 
-    for a in soup.findAll('div',attrs={'jscontroller':'d0DtYd'}):
-        title=a.find('a', attrs={'class':'DY5T1d'})
-        newssite=a.find('a', attrs={'class':'wEwyrc AVN2gc uQIVzc Sksgp'})
-        date=a.find('time')
+    if websiteName == "https://www.news.google.com":
+        print("google")
+        main_tag = 'div'
+        main_attrs = {'jscontroller':'d0DtYd'}
+        title_tag = 'a'
+        title_attrs = {'class':'DY5T1d'}
+        newssite_tag = 'a'
+        newssite_attrs = {'class':'wEwyrc AVN2gc uQIVzc Sksgp'}
+        date_tag = 'time'
+    elif websiteName == "https://uk.news.yahoo.com":
+        print("yahoo")
+        main_tag = 'div'
+        main_attrs = {'class':'Ov(h) Pend(44px) Pstart(25px)'}
+        title_tag = 'a'
+        title_attrs = {'class':'Fw(b) Fz(20px) Lh(23px) Fz(17px)--sm1024 Lh(19px)--sm1024 mega-item-header-link Td(n) C(#0078ff):h C(#000) LineClamp(2,46px) LineClamp(2,38px)--sm1024 not-isInStreamVideoEnabled'}
+        newssite_tag = 'div'
+        newssite_attrs = {'class':'C(#959595) Fz(11px) D(ib) Mb(6px)'}
+        date_tag = 'div'
+    elif websiteName == "https://www.bing.com/news":
+        print("bing")
+        main_tag = 'div'
+        main_attrs = {'class':'caption'}
+        title_tag = 'a'
+        title_attrs = {'class':'title'}
+        newssite_tag = 'a'
+        newssite_attrs = {'aria-label':re.compile('Search news from .*')}
+        date_tag = 'span'
+    else:
+        print("no matching website")
 
-        el=[title,newssite,date]
+    # with open("temp.txt", 'w') as file:
+    #     file.write(soup.prettify().encode('utf-8'))
+    #print(soup.prettify())
+
+    for a in soup.findAll(main_tag, attrs=main_attrs):
+        #print("in findAll loop")
+        title=a.find(title_tag, attrs=title_attrs).text
+        newssite=a.find(newssite_tag, attrs=newssite_attrs).text
+        date=a.find(date_tag).text
+
+        el={"title": title, "newssite": newssite, "date": date}
         elements.append(el)
 
-    elements_print(elements)
     return elements
 
 def elements_print(elements):
     for el in elements:
-        for a in el:
-            print(a.text)
+        print(el["title"])
+        print(el["newssite"])
+        print(el["date"])
         print('\n')
 
 
