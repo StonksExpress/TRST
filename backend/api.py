9fd269d8437fcda4dc6fc6ff529763f60d6122be
Ivan Ereshchenko <ivan.ereshchenko18@imperial.ac.uk>
2020-02-08 22:22:20 +0000

    changed virtually nothing

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 import json
+import text_similarity.py as sim
 
 app = Flask(__name__)
 CORS(app)
@@ -8,6 +9,7 @@ app.config['DEBUG'] = True
 
 @app.route('/api/xml', methods=['GET'])
 def get_xml():
+    url = request.args.get('url')
 
     return {"success": False}
 
