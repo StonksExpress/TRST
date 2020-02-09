30a116821d5b2be9fd6b72b122b5f0f948dacbd0
jrpdotjpg <jrp2018@ic.ac.uk>
2020-02-09 06:26:52 +0000

    time stuff, NO RETURN YET in total similaritt

--- backend/text_similarity.py
+++ backend/text_similarity.py
@@ -3,6 +3,7 @@ import tensorflow_hub as hub
 import sklearn.metrics.pairwise as pw
 import numpy as np
 import nltk
+import math
 from newspaper import Article
 
 class embeddings:
@@ -38,16 +39,49 @@ class embeddings:
         document_sentence_embeddings = session.run(embedded_text, feed_dict={text_input: document_sentences})
         return document_sentence_embeddings
 
-    def cosine_embed_similarity(embeddings1, embeddings2):
-        #return pw.cosine_similarity(embeddings1, embeddings2)
+def cosine_pair_embed_similarity(embeddings1, embeddings2):
+    #return pw.cosine_similarity(embeddings1, embeddings2)
 
-        arr_big, arr_small = (np.array(embeddings1),np.array(embeddings2)) if len(embeddings1) > len(embeddings2) else (np.array(embeddings2),np.array(embeddings1))
-        similarities = [max(arr_big,key=lambda big_embed: 1 - spatial.distance.cosine(big_embed, small_embed)) for small_embed in arr_small]
+    arr_big, arr_small = (np.array(embeddings1),np.array(embeddings2)) if len(embeddings1) > len(embeddings2) else (np.array(embeddings2),np.array(embeddings1))
+    similarities = [max(arr_big,key=lambda big_embed: 1 - spatial.distance.cosine(big_embed, small_embed)) for small_embed in arr_small]
 
+def total_similarity(timestamp,embedding,embeddings,embeddings_times,sf_norm=0.1,sf_dropoff=0.1):
+    #use kernel of size a day
+    #convolve with points
+    #scale with exponential
+    #
+        #k + k*s(-x)
+    zipped_and_sorted = sorted(zip(embeddings,embeddings_times), key=lambda x: x[1])
+    results_similarity = [cosine_pair_embed_similarity(embedding,x[0]) for x in zipped_and_sorted]
 
-def generate_keywords(url):
-    article = Article(url)
-    article.download()
-    article.parse()
-    article.nlp()
-    return article.keywords
+    #time stuff
+    left = filter(zipped_and_sorted, lambda x: x[1] < timestamp)
+    right = zipped_and_sorted[len(left):]
+    left_times = [x[1] for x in left]
+    left_data = [x[0] for x in left]
+    right_data = [x[0] for x in right]
+    right_times = [x[1] for x in right]
+
+    left_time_diffs = [timestamp-x[1] for x in reversed(left)]
+    right_time_diffs = [x[1]-timestamp for x in right]
+
+    inter_right_time_diffs = [b-a for (a,b) in zip(right_times.insert(0,timestamp),right_times.insert(0,timestamp)[1:])]
+    inter_left_time_diffs = [b-a for (a,b) in zip(left_times.append(timestamp),left_times.append(timestamp)[1:])]
+    max_diff = max(inter_left_time_diffs + inter_right_time_diffs)
+
+#   scaling factor depending on density
+    #sf_norm=0.1
+    norm_scaling_left = [1-math.exp(sf*max_diff/x) for x in left_time_diffs]
+    norm_scaling_right = [1-math.exp(sf*max_diff/x) for x in right_time_diffs]
+
+#   scaling factor depending on distance from queried
+    #sf_dropoff=0.1
+    time_scaling_func = lambda x: math.exp(-sf_dropoff*x)
+    scaled_similarities = [x*y for (x,y) in zip(left_data+right_data,norm_scaling_left+norm_scaling_right)]
+    scaled_similarities = [x*time_scaling_func(y) for (x,y) in zip(scaled_similarities,left_time_diffs+right_time_diffs)]
+
+def get_score(url):
+    #check if present in db
+        #return db value if true
+    #else
+    #
